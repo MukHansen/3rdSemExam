ea699ebec09a17c7a039965b84aa17d17e4c1cb4
Mads <cph-mh752@cphbusiness.dk>
2020-01-14 04:01:57 +0100

    Attempt to deploy frontend via backend, if succesfull --> FINAL COMMIT

---
+++ src/main/webapp/precache-manifest.67b00efb5e771010780f575649370ed3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a3e59c1c2dcf7d9bb84bab2b70e23be",
+    "url": "/3rdSemExam/index.html"
+  },
+  {
+    "revision": "4819dbc7d86cee509809",
+    "url": "/3rdSemExam/static/css/main.08622119.chunk.css"
+  },
+  {
+    "revision": "4529c9f89d0f877c30ce",
+    "url": "/3rdSemExam/static/js/2.6cc458aa.chunk.js"
+  },
+  {
+    "revision": "4819dbc7d86cee509809",
+    "url": "/3rdSemExam/static/js/main.58b6ca10.chunk.js"
+  },
+  {
+    "revision": "b6f27aaf9d71a9e67528",
+    "url": "/3rdSemExam/static/js/runtime-main.b4e3eb3d.js"
+  }
+]);
